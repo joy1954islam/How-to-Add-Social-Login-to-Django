99cd6a0442c4b07559f085bc8ffff341a5ddfc00
joyislam1954 <joyislam1954@gmail.com>
2020-06-20 09:44:06 +0600

    Django Social Login

--- Django_Social_Login/urls.py
+++ Django_Social_Login/urls.py
@@ -14,13 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Social_Apps import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home, name='home'),
+    # path('',views.Base, name='Base'),
     path('', auth_views.LoginView.as_view(template_name='Social_Apps/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Social_Apps/logout.html'), name='logout'),
+    path('sign-up/',views.register, name='sign_up'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='Social_Apps/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='Social_Apps/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='Social_Apps/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='Social_Apps/password_reset_complete.html'), name='password_reset_complete'),
+    path('change/profile/', views.ChangeProfile, name='change_profile'),
+
+    path('settings/', views.SettingsView.as_view(), name='settings'),
+    path('settings/password/', views.password, name='password'),
+    path('oauth/', include('social_django.urls', namespace='social')),
+
 ]
--- Social_Apps/views.py
+++ Social_Apps/views.py
@@ -1,7 +1,92 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from social_django.models import UserSocialAuth
+from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from Social_Apps.forms import UserRegisterForm, UserUpdateForm
+from django.contrib import messages
 
 
 def home(request):
     return render(request,'Social_Apps/Home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'Social_Apps/register.html', {'form': form})
+
+
+class SettingsView(LoginRequiredMixin, TemplateView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+
+        try:
+            github_login = user.social_auth.get(provider='github')
+        except UserSocialAuth.DoesNotExist:
+            github_login = None
+
+        try:
+            twitter_login = user.social_auth.get(provider='twitter')
+        except UserSocialAuth.DoesNotExist:
+            twitter_login = None
+
+        try:
+            facebook_login = user.social_auth.get(provider='facebook')
+        except UserSocialAuth.DoesNotExist:
+            facebook_login = None
+
+        can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
+
+        return render(request, 'Social_Apps/settings.html', {
+            'github_login': github_login,
+            'twitter_login': twitter_login,
+            'facebook_login': facebook_login,
+            'can_disconnect': can_disconnect
+        })
+
+
+@login_required
+def password(request):
+    if request.user.has_usable_password():
+        PasswordForm = PasswordChangeForm
+    else:
+        PasswordForm = AdminPasswordChangeForm
+
+    if request.method == 'POST':
+        form = PasswordForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordForm(request.user)
+    return render(request, 'Social_Apps/password.html', {'form': form})
+
+
+@login_required
+def ChangeProfile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,request.FILES, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('home')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {
+        'u_form': u_form
+    }
+    return render(request, 'Social_Apps/change_profile.html', context)
